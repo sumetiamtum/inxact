213a05cbfdbd30d54c25a52fcea33370971d9f63
sumetiamtum <sumetiamtum@gmail.com>
2015-10-03 14:53:01 +0100

    setup-db.sh

---
+++ setup-db.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+function remove_unneeded {
+    # Some Debian have portmap installed. We don't need that.
+    check_remove /sbin/portmap portmap
+
+    # Remove rsyslogd, which allocates ~30MB privvmpages on an OpenVZ system,
+    # which might make some low-end VPS inoperatable. We will do this even
+    # before running apt-get update.
+    check_remove /usr/sbin/rsyslogd rsyslog
+
+    # Other packages that seem to be pretty common in standard OpenVZ
+    # templates.
+    check_remove /usr/sbin/apache2 'apache2*'
+    check_remove /usr/sbin/named bind9
+    check_remove /usr/sbin/smbd 'samba*'
+    check_remove /usr/sbin/nscd nscd
+
+    # Need to stop sendmail as removing the package does not seem to stop it.
+    if [ -f /usr/lib/sm.bin/smtpd ]
+    then
+        invoke-rc.d sendmail stop
+        check_remove /usr/lib/sm.bin/smtpd 'sendmail*'
+    fi
+}
+
+function update_upgrade {
+    # Run through the apt-get update/upgrade first. This should be done before
+    # we try to install any package
+    apt-get -q -y update
+    apt-get -q -y upgrade
+}
+
+function install_dash {
+    check_install dash dash
+    rm -f /bin/sh
+    ln -s dash /bin/sh
+}
+
+function install_dropbear {
+    check_install dropbear dropbear
+    check_install /usr/sbin/xinetd xinetd
+
+    # Disable SSH
+    touch /etc/ssh/sshd_not_to_be_run
+    invoke-rc.d ssh stop
+
+    # Enable dropbear to start. We are going to use xinetd as it is just
+    # easier to configure and might be used for other things.
+    cat > /etc/xinetd.d/dropbear <<END
+service ssh
+{
+    socket_type     = stream
+    only_from       = 0.0.0.0
+    wait            = no
+    user            = root
+    protocol        = tcp
+    server          = /usr/sbin/dropbear
+    server_args     = -i
+    disable         = no
+}
+END
+    invoke-rc.d xinetd restart
+}
+
+
+########################################################################
+# START OF PROGRAM
+########################################################################
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+check_sanity
+case "$1" in
+exim4)
+    install_exim4
+    ;;
+mysql)
+    install_mysql
+    ;;
+nginx)
+    install_nginx
+    ;;
+php)
+    install_php
+    ;;
+system)
+    remove_unneeded
+    update_upgrade
+    install_dash
+    install_syslogd
+    install_dropbear
+    ;;
+wordpress)
+    install_wordpress $2
+    ;;
+*)
+    echo 'Usage:' `basename $0` '[option]'
+    echo 'Available option:'
+    for option in system exim4 mysql nginx php wordpress
+    do
+        echo '  -' $option
+    done
+    ;;
+esac
