4bf693f44e0c0bb422899fd2b4438bcd0262bf0d
sumetiamtum <sumetiamtum@gmail.com>
2019-03-04 20:49:51 +0000

    Update upgrade-jessie-to-stretch.sh

--- upgrade-jessie-to-stretch.sh
+++ upgrade-jessie-to-stretch.sh
@@ -13,18 +13,31 @@ function check_sanity {
         die "Distribution is not supported"
     fi
 }
-
+ 	echo Update sources
 	apt-get -q -y update
+	echo Upgrade what can be upgraded
 	apt-get -q -y upgrade
+	echo Now the Distribution upgrade
 	apt-get -q -y dist-upgrade
+	echo Remove the Sources.List - they include old sources
 	rm /etc/apt/sources.list
+	echo Done. Now create new sources
+	echo http://httpredir.debian.org/debian stretch main
+	echo http://httpredir.debian.org/debian stretch main
+	echo http://security.debian.org stretch/updates main
 	cat > sources.list << EOF
 deb http://httpredir.debian.org/debian stretch main
 deb http://httpredir.debian.org/debian stretch-updates main
 deb http://security.debian.org stretch/updates main
 EOF
+	echo Moving sources list to the /etc/apt folder
 	mv sources.list /etc/apt/
+	echo Update new sources
 	apt-get -q -y update
+	echo Upgrade everything again
 	apt-get -q -y upgrade
+	echo Upgrade the distribution to Stretch
 	apt-get dist-upgrade
+	echo About to reboot
+	sleep 3
 	reboot
