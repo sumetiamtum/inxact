a86a7cf7cf72539a0056613e488d02e9d3eb59bd
sumetiamtum <sumetiamtum@gmail.com>
2019-03-04 20:53:12 +0000

    Update upgrade-jessie-to-stretch.sh

--- upgrade-jessie-to-stretch.sh
+++ upgrade-jessie-to-stretch.sh
@@ -13,31 +13,31 @@ function check_sanity {
         die "Distribution is not supported"
     fi
 }
- 	echo Update sources
+ 	echo -e "\e[31Update sources\e[0m"
 	apt-get -q -y update
-	echo Upgrade what can be upgraded
+	echo -e "\e[31Upgrade what can be upgraded\e[0m"
 	apt-get -q -y upgrade
-	echo Now the Distribution upgrade
+	echo -e "\e[31Now the Distribution upgrade\e[0m"
 	apt-get -q -y dist-upgrade
-	echo Remove the Sources.List - they include old sources
+	echo -e "\e[31Removing the old sources.list - they include old sources\e[0m"
 	rm /etc/apt/sources.list
-	echo Done. Now create new sources
-	echo http://httpredir.debian.org/debian stretch main
-	echo http://httpredir.debian.org/debian stretch main
-	echo http://security.debian.org stretch/updates main
+	echo -e "\e[31Done. Now create new sources\e[0m"
+	echo -e "\e[31http://httpredir.debian.org/debian stretch main\e[0m"
+	echo -e "\e[31http://httpredir.debian.org/debian stretch main\e[0m"
+	echo -e "\e[31http://security.debian.org stretch/updates main\e[0m"
 	cat > sources.list << EOF
 deb http://httpredir.debian.org/debian stretch main
 deb http://httpredir.debian.org/debian stretch-updates main
 deb http://security.debian.org stretch/updates main
 EOF
-	echo Moving sources list to the /etc/apt folder
+	echo -e "\e[31Moving sources list to the /etc/apt folder\e[0m"
 	mv sources.list /etc/apt/
-	echo Update new sources
+	echo -e "\e[31Update new sources\e[0m"
 	apt-get -q -y update
-	echo Upgrade everything again
+	echo -e "\e[31Upgrade everything again\e[0m"
 	apt-get -q -y upgrade
-	echo Upgrade the distribution to Stretch
+	echo -e "\e[31Upgrade the distribution to Stretch\e[0m"
 	apt-get dist-upgrade
-	echo About to reboot
+	echo -e "\e[31About to reboot\e[0m"
 	sleep 3
 	reboot
