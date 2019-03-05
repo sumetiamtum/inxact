ccd263150149c37ea2fb0921d10908b6ce9fbc4b
sumetiamtum <sumetiamtum@gmail.com>
2019-03-05 14:55:55 +0000

    Update install_https_certificates_setup_auto_renewal.sh

--- install_https_certificates_setup_auto_renewal.sh
+++ install_https_certificates_setup_auto_renewal.sh
@@ -1,4 +1,4 @@
-# Install the certificates for all the domains on the server
+# Install the certificates for all the domains on the server NOTE THIS DOESNT WORK - DO IT MANUALLY
 sudo certbot --apache -d $1 -d www.$1 -d $2 -d www.$2 -d $3 -d www.$3
 # Setup cronjob for automatic renewal 
 (crontab -l ; echo "30 2 */2 * * /usr/bin/certbot renew >> /var/log/letsencrypt/renew.log")| crontab -
